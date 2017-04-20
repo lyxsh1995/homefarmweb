c56d5f8b98993b23481dcb0521001c8f6aa54919
lyxsh2016 <330653924@qq.com>
2017-04-20 11:43:15 +0800

    2017年4月20日11:42:47

---
+++ 城市农场/js/myjson.js
@@ -0,0 +1,49 @@
+function myjson() {
+	this.FInterID;
+
+	this.FBillNo;
+
+	this.FOnOrOff = "1";
+
+	this.FFloorOne = "0";
+
+	this.FFloorTwo = "0";
+
+	this.FFloorThree = "0";
+
+	this.FDouyaji = "0";
+
+	this.FMG = "0";
+
+	this.FShuiXiang = "0";
+
+	this.FTypeID;
+
+	this.FSourceType = "3";
+
+	this.FFreq = "0";
+
+	this.FContinuePM;
+
+	this.FCorS = "0";
+
+	this.zhouqijiange = "0";
+
+	this.kaishitime = "0";
+
+	this.begintime = "0";
+
+	this.endtime = "0";
+
+	this.firsttime = "0";
+
+	this.FExcTime = "0";
+
+	this.FStatus = "0";
+
+	this.FEndReason = "0";
+
+	this.EQID;
+
+	this.EQIDMD5;
+}
